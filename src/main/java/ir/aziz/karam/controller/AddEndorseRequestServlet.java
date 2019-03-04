850cac3953bd73e21a23882df932705908625fa9
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2019-03-04 22:41:16 +0330

    Fixbug on resending endorment form to server

--- src/main/java/ir/aziz/karam/controller/AddEndorseRequestServlet.java
+++ src/main/java/ir/aziz/karam/controller/AddEndorseRequestServlet.java
@@ -12,6 +12,7 @@ import ir.aziz.karam.model.manager.UserManager;
 import ir.aziz.karam.model.types.Endorse;
 import ir.aziz.karam.model.types.Skill;
 import ir.aziz.karam.model.types.User;
+
 import java.io.IOException;
 import java.util.Map;
 import javax.servlet.ServletException;
@@ -19,6 +20,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.apache.log4j.Logger;
 
 @WebServlet("/addEndorse/*")
@@ -36,16 +38,23 @@ public class AddEndorseRequestServlet extends HttpServlet {
             String skillName = parameterMap.get("skillName")[0];
             User user = UserManager.getInstance().getUserById(userId);
             User currentUser = UserManager.getInstance().getUserById(currentUserId);
-            currentUser.addEndorses(new Endorse(userId, skillName));
-            Skill skillOfUserBySkillName = UserManager.getInstance().getSkillOfUserBySkillName(user, skillName);
-            skillOfUserBySkillName.setPoints(skillOfUserBySkillName.getPoints() + 1);
-            request.setAttribute("user", user);
-            request.setAttribute("skills", SkillManager.getInstance().getAllSkills());
-            request.setAttribute("currenUser", UserManager.getInstance().getCurrentUser());
-            if (UserManager.getInstance().getCurrentUser().getId().equals(userId)) {
-                request.getRequestDispatcher("/user-single-logged-in.jsp").forward(request, response);
+            if (!UserManager.getInstance().userEndorseThisEndorse(currentUser, user.getId(), skillName)) {
+                currentUser.addEndorses(new Endorse(userId, skillName));
+                Skill skillOfUserBySkillName = UserManager.getInstance().getSkillOfUserBySkillName(user, skillName);
+                skillOfUserBySkillName.setPoints(skillOfUserBySkillName.getPoints() + 1);
+
+                request.setAttribute("user", user);
+                request.setAttribute("skills", SkillManager.getInstance().getAllSkills());
+                request.setAttribute("currenUser", UserManager.getInstance().getCurrentUser());
+                if (UserManager.getInstance().getCurrentUser().getId().equals(userId)) {
+                    request.getRequestDispatcher("/user-single-logged-in.jsp").forward(request, response);
+                } else {
+                    request.getRequestDispatcher("/user-single-guest.jsp").forward(request, response);
+                }
             } else {
-                request.getRequestDispatcher("/user-single-guest.jsp").forward(request, response);
+                response.setStatus(403);
+                request.setAttribute("message", "skill id already endorsed.");
+                request.getRequestDispatcher("/permission-denied403.jsp").forward(request, response);
             }
         } catch (UserNotFoundException | SkillNotFoundException ex) {
             Logger.getLogger(AddSkillUserRequestServlet.class).error(ex, ex);
