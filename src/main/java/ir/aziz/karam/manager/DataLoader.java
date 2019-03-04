ac6e876be2ac4fbe1c13be5679dff53e786a5032
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2019-03-04 18:53:13 +0330

    Fixbug for encoding on data retrived from network

--- src/main/java/ir/aziz/karam/manager/DataLoader.java
+++ src/main/java/ir/aziz/karam/manager/DataLoader.java
@@ -12,6 +12,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class DataLoader {
@@ -20,7 +21,7 @@ public class DataLoader {
         StringBuilder result = new StringBuilder();
         URL oracle = new URL(url);
         try (BufferedReader in = new BufferedReader(
-                new InputStreamReader(oracle.openStream()))) {
+                new InputStreamReader(oracle.openStream(), StandardCharsets.UTF_8))) {
             String inputLine;
             while ((inputLine = in.readLine()) != null)
                 result.append(inputLine);
