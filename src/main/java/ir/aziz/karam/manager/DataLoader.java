d0c5c8723f732e51fb275fcb0aa5796d923430d3
mahdikarami77 <mahdikarami77@ut.ac.ir>
2019-03-18 23:41:21 +0330

    commit trash

--- src/main/java/ir/aziz/karam/manager/DataLoader.java
+++ src/main/java/ir/aziz/karam/manager/DataLoader.java
@@ -5,14 +5,11 @@
  */
 package ir.aziz.karam.manager;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.List;
 
 public class DataLoader {
 
