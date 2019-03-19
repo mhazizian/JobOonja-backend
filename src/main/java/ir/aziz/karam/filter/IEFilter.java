ab3eaef6f2fe91462e062ff89bb44f711cec289b
mahdikarami77 <mahdikarami77@ut.ac.ir>
2019-03-19 19:28:53 +0330

    set header

--- src/main/java/ir/aziz/karam/filter/IEFilter.java
+++ src/main/java/ir/aziz/karam/filter/IEFilter.java
@@ -14,6 +14,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 @WebFilter("/*")
@@ -26,9 +27,14 @@ public class IEFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse rec = (HttpServletResponse) response;
         String path = req.getServletPath();
         long st = System.currentTimeMillis();
+        MutableHttpServletRequest requestWrapper = new MutableHttpServletRequest(req);
+        MutableHttpServletResponse responseWrapper = new MutableHttpServletResponse(rec);
+        requestWrapper.putHeader("Access-Control-Allow-Methods", "*");
         chain.doFilter(request, response);
+        responseWrapper.putHeader("Access-Control-Allow-Methods", "*");
         long et = System.currentTimeMillis();
         long tt = (et - st);
         Logger.getLogger(IEFilter.class).info("Time take to execute action " + path + "   is  :  " + tt);
