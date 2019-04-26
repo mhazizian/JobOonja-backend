147e893df72ecc322a5f30cd24e4c16cd7041637
mahdikarami77 <mahdikarami77@ut.ac.ir>
2019-04-26 14:41:53 +0430

    Add Testfile for testing functionality of JDBC

---
+++ src/main/java/TEST.java
@@ -0,0 +1,14 @@
+
+import ir.aziz.karam.dataLayer.dataMappers.project.ProjectMapper;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+public class TEST {
+    public static void main(String[] args) throws SQLException {
+        ProjectMapper.getInstance();
+    }
+}
