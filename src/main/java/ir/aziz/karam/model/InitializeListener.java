be5a7622eb0d8e75f627224f4825cf439325dc5f
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2019-04-28 16:02:20 +0430

    Add InitializeListener to initial DB connection and retrive data from server on Server starts

---
+++ src/main/java/ir/aziz/karam/model/InitializeListener.java
@@ -0,0 +1,45 @@
+package ir.aziz.karam.model;
+
+import ir.aziz.karam.model.dataLayer.dataMappers.bid.BidMapper;
+import ir.aziz.karam.model.dataLayer.dataMappers.endorsment.EndorsmentMapper;
+import ir.aziz.karam.model.dataLayer.dataMappers.project.ProjectMapper;
+import ir.aziz.karam.model.dataLayer.dataMappers.skill.SkillMapper;
+import ir.aziz.karam.model.dataLayer.dataMappers.skillUser.SkillUserMapper;
+import ir.aziz.karam.model.dataLayer.dataMappers.user.UserMapper;
+import ir.aziz.karam.model.manager.SkillManager;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebListener
+public class InitializeListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("On start web app");
+        try {
+            SkillMapper.getInstance();
+
+            ProjectMapper.getInstance();
+            UserMapper.getInstance();
+
+            SkillUserMapper.getInstance();
+            EndorsmentMapper.getInstance();
+            BidMapper.getInstance();
+
+            SkillManager.getInstance().updateSkillListFromServer();
+
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("On shutdown web app");
+    }
+
+}
